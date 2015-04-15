71836c755abfa9a86be6903b21fb595c02e59f6c
Gregory <gigozzo4@gmail.com>
2015-04-15 16:41:25 +0300

    Add model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Statistics(models.Model):
+    dt = models.DateTimeField(primary_key=True)
+    spam = models.IntegerField()
+    notspam = models.IntegerField()
+    reverts = models.IntegerField()
+    complaints = models.IntegerField()
+    total = models.IntegerField()
+    class Meta:
+        managed = False
+        db_table = 'statistics'
