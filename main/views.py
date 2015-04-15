c8a63c49f5e84794e8a469dc011c162feb1fa15b
Gregory <gigozzo4@gmail.com>
2015-04-15 18:10:20 +0300

    Add dump

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.views.generic import Statistics
 
 # Create your views here.
+
+class StatisticsView(Statistics): # представление в виде списка
+    model = Post
