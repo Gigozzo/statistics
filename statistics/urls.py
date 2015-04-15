63cbfdfcf6b9d5949718df389312a7b5b19cecb6
Gregory <gigozzo4@gmail.com>
2015-04-15 19:55:55 +0300

    Add urls

--- statistics/urls.py
+++ statistics/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from main.views import StatisticsView
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'statistics.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', StatisticsView.as_view(), name='main'),
+                       url(r'^admin/', include(admin.site.urls)),
 )
